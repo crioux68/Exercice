9ffa00b28b8ffb4efdb928acd2b7f2ce02500246
Borel Giovanni NTI AKOUMBA <48840415+borelaknti@users.noreply.github.com>
2024-01-26 14:28:19 -0500

    traduction fonctionnelle  coommentaires#1

--- trad.py
+++ trad.py
@@ -3,33 +3,20 @@
 
 from langdetect import detect
 from deep_translator import GoogleTranslator
-""" 
-
-
-def detecter_langue(texte):
-    langue_detectee = GoogleTranslator(source='auto').detect(texte)
-    return langue_detectee
-
-# Exemple d'utilisation de la fonction
-texte = "Hello, how are you?"
-langue_detectee = detecter_langue(texte)
-print("Langue détectée :", langue_detectee) """
-
-
-
-
-# Fonction pour détecter automatiquement la langue d'un texte
-def detecter_langue(texte):
-    langue_detectee = detect(texte)
-    return langue_detectee
 
 # Exemple d'utilisation de la fonction
 textTraduit = str
 textATraduire = "Guten Morgen"
 langue = "en"
 
+# Fonction pour détecter automatiquement la langue d'un texte qui est passé en paramètre
+def DetecterLangue(texte):
+    langue_detectee = detect(texte)
+    return langue_detectee
+
+# Fonction qui prend le texte à traduire, la langue de retour  et retourne le texte traduit
 def Traduction(textATraduire,langue):
-    langue_detectee = detecter_langue(textATraduire)
+    langue_detectee = DetecterLangue(textATraduire)
     textTraduit = GoogleTranslator(source=langue_detectee,target=langue).translate(text=textATraduire)
     return textTraduit 
 
